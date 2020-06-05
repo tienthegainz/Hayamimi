4678c55447400486c471276c2d7feafd1d28db5f
Tien <viettienha98@gmail.com>
2020-06-05 15:08:36 +0700

    Code style config

--- frontend/src/Authenticate/Register.js
+++ frontend/src/Authenticate/Register.js
@@ -1,110 +1,108 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
-    Form,
-    Input,
-    Button,
-    Radio,
-    Select,
-    Cascader,
-    DatePicker,
-    InputNumber,
-    TreeSelect,
-    Switch,
-} from 'antd';
-import { withRouter } from 'react-router-dom'
+  Form,
+  Input,
+  Button,
+  Radio,
+  Select,
+  Cascader,
+  DatePicker,
+  InputNumber,
+  TreeSelect,
+  Switch,
+} from "antd";
+import { withRouter } from "react-router-dom";
 
 const layout = {
-    labelCol: {
-        span: 8,
-    },
-    wrapperCol: {
-        span: 8,
-    },
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 8,
+  },
 };
 const tailLayout = {
-    wrapperCol: {
-        offset: 8,
-        span: 8,
-    },
+  wrapperCol: {
+    offset: 8,
+    span: 8,
+  },
 };
 
 function Register() {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
 
-    const onFinish = values => {
-        console.log('Success:', values);
-    };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
 
-    const onFinishFailed = errorInfo => {
-        console.log('Failed:', errorInfo);
-    };
+  return (
+    <div>
+      <br></br>
+      <br></br>
+      <Form
+        {...layout}
+        name="basic"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: "Please input your email!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-    return (
-        <div>
-            <br></br><br></br>
-            < Form
-                {...layout}
-                name="basic"
-                initialValues={
-                    {
-                        remember: true,
-                    }
-                }
-                onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
-            >
-                <Form.Item
-                    label="Email"
-                    name="email"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your email!',
-                        },
-                    ]}
-                >
-                    <Input />
-                </Form.Item>
+        <Form.Item
+          label="Nickname"
+          name="nickname"
+          rules={[
+            {
+              required: true,
+              message: "Please input your nickname!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-                <Form.Item
-                    label="Nickname"
-                    name="nickname"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your nickname!',
-                        },
-                    ]}
-                >
-                    <Input />
-                </Form.Item>
-
-                <Form.Item
-                    label="Password"
-                    name="password"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your password!',
-                        },
-                    ]}
-                >
-                    <Input.Password />
-                </Form.Item>
-                <Form.Item label="Birthday">
-                    <DatePicker />
-                </Form.Item>
-                <Form.Item {...tailLayout}>
-                    <Button type="primary" htmlType="submit">
-                        Register
-                    </Button>
-                    &emsp;&emsp;
-                    <Button type="default" href="/login">
-                        Go to Login
-                    </Button>
-                </Form.Item>
-            </Form>
-        </div>
-    );
-};
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item label="Birthday">
+          <DatePicker />
+        </Form.Item>
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            Register
+          </Button>
+          <br></br>
+          <Button type="link" href="/login">
+            Go to Login
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+}
 
 export default withRouter(Register);
