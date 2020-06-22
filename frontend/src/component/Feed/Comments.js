93ec02d9b073d4c410eb66710a0e399703095246
Hà Việt Tiến <34891363+tienthegainz@users.noreply.github.com>
2020-06-22 10:46:55 +0700

    Sort query and Time in comment (#32)

--- frontend/src/component/Feed/Comments.js
+++ frontend/src/component/Feed/Comments.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { Comment, Avatar, Form, Button, List, Input } from 'antd';
-import FirebaseController from '../../firebase.js'
+import React, { useState, useEffect } from "react";
+import { Comment, Avatar, Form, Button, List, Input } from "antd";
+import FirebaseController from "../../firebase.js";
 
 const { TextArea } = Input;
 
@@ -23,16 +23,15 @@ const Editor = ({ onChange, onSubmit, submitting, value }) => (
 );
 
 const Comments = (props) => {
-
-  console.log(props)
+  console.log(props);
 
   const pid = props.pid;
   const [commentsID, setCommentsID] = useState(props.commentsID);
   const [comments, setComments] = useState([]);
   const [submitting, setSubmitting] = useState(false);
-  const [value, setValue] = useState('');
-  const avatar = localStorage.getItem('avatar');
-  const displayName = localStorage.getItem('displayName');
+  const [value, setValue] = useState("");
+  const avatar = localStorage.getItem("avatar");
+  const displayName = localStorage.getItem("displayName");
   const uid = localStorage.getItem("uid");
 
   const CommentList = (
@@ -40,13 +39,13 @@ const Comments = (props) => {
       className="comment-list"
       itemLayout="horizontal"
       dataSource={comments}
-      renderItem={item => (
+      renderItem={(item) => (
         <li>
           <Comment
             author={item.displayName}
             avatar={item.avatarURL}
             content={item.content}
-          // datetime={item.date}
+            datetime={item.date}
           />
         </li>
       )}
@@ -59,24 +58,42 @@ const Comments = (props) => {
 
   const getComment = async () => {
     console.log("Get Comment of ", pid);
-    const usersRef = await FirebaseController.db
-      .collection('users')
+    const usersRef = await FirebaseController.db.collection("users").get();
+
+    const commentsRef = await FirebaseController.db
+      .collection("comments")
+      .where("pid", "==", props.pid)
+      .orderBy("date", "asc")
       .get();
 
-    const commentsRef = await FirebaseController.db.collection('comments').where('pid', '==', props.pid).get();
-
-    const usersSnapshot = await usersRef.docs.map((doc) => ({ uid: doc.data().uid, displayName: doc.data().displayName, avatarURL: doc.data().avatarURL }));
+    const usersSnapshot = await usersRef.docs.map((doc) => ({
+      uid: doc.data().uid,
+      displayName: doc.data().displayName,
+      avatarURL: doc.data().avatarURL,
+    }));
 
     const commentsSnapshot = commentsRef.docs.map((doc) => ({
-      pid: doc.id, uid: doc.data().uid, content: doc.data().content,
-      date: doc.data().date.toDate()
+      pid: doc.id,
+      uid: doc.data().uid,
+      content: doc.data().content,
+      date: new Intl.DateTimeFormat("en-US", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+      }).format(doc.data().date.toDate()),
     }));
-    console.log('CmtSnap:', commentsSnapshot);
+    console.log("CmtSnap:", commentsSnapshot);
     let data = [];
 
     commentsSnapshot.forEach((snapshot) => {
-      snapshot.displayName = usersSnapshot.find(e => (e.uid === snapshot.uid)).displayName;
-      snapshot.avatarURL = usersSnapshot.find(e => (e.uid === snapshot.uid)).avatarURL;
+      snapshot.displayName = usersSnapshot.find(
+        (e) => e.uid === snapshot.uid
+      ).displayName;
+      snapshot.avatarURL = usersSnapshot.find(
+        (e) => e.uid === snapshot.uid
+      ).avatarURL;
       data.push(snapshot);
       console.log(data);
     });
@@ -87,11 +104,12 @@ const Comments = (props) => {
     if (!value) {
       return;
     }
+    const date = new Date();
     let push_data = {
       pid: pid,
       uid: uid,
       content: value,
-      date: new Date(),
+      date: date,
     };
 
     // Add comments
@@ -100,33 +118,36 @@ const Comments = (props) => {
       .add(push_data)
       .then((ref) => {
         console.log("Added document with ID: ", ref.id);
-        setCommentsID([...commentsID, ref.id])
+        setCommentsID([...commentsID, ref.id]);
         FirebaseController.db
           .collection("posts")
-          .doc(pid).update({
-            commentID: [...commentsID, ref.id]
-          })
+          .doc(pid)
+          .update({
+            commentID: [...commentsID, ref.id],
+          });
       });
 
     let comment_data = {
       pid: pid,
       uid: uid,
       content: value,
-      date: new Date(),
+      date: new Intl.DateTimeFormat("en-US", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+      }).format(date),
       displayName: displayName,
-      avatarURL: avatar
-    }
+      avatarURL: avatar,
+    };
 
     setSubmitting(true);
     setTimeout(() => {
       setSubmitting(false);
-      setValue('');
+      setValue("");
 
-      setComments([
-        ...comments,
-        comment_data
-      ]);
-
+      setComments([...comments, comment_data]);
     }, 1000);
   };
 
@@ -136,15 +157,9 @@ const Comments = (props) => {
 
   return (
     <>
-      {(commentsID.length > 0) ? CommentList : <div></div>}
+      {commentsID.length > 0 ? CommentList : <div></div>}
       <Comment
-        avatar={
-          <Avatar
-            src={avatar}
-            alt={displayName}
-
-          />
-        }
+        avatar={<Avatar src={avatar} alt={displayName} />}
         content={
           <Editor
             onChange={handleChange}
