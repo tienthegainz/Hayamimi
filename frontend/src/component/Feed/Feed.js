c2089a7fc35e89e894d709460759b30eac1996ae
Tien <viettienha98@gmail.com>
2020-06-22 17:05:15 +0700

    Real_time but break other things. DO NOT PR

--- frontend/src/component/Feed/Feed.js
+++ frontend/src/component/Feed/Feed.js
@@ -6,29 +6,105 @@ import FirebaseController from "../../firebase.js";
 
 const Feed = (props) => {
   const [Posts, setPosts] = useState([]);
+  const [usersOnPost, setUsersOnPost] = useState([]);
   const currentUID = localStorage.getItem("uid");
   const type = props.type;
   const urlUid = props.uid;
   const isAdmin = localStorage.getItem("isAdmin");
 
-  useEffect(() => {
-    getPosts();
-  }, []);
+  useEffect(async () => {
+    const postsRef =
+      type == "profile"
+        ? await FirebaseController.db
+          .collection("posts")
+          .where("uid", "==", urlUid)
+          .orderBy("date", "desc")
+        : type == "search"
+          ? await FirebaseController.db
+            .collection("posts")
+            .orderBy("date", "desc")
+          : await FirebaseController.db
+            .collection("posts")
+            .orderBy("date", "desc");
+
+
+
+    const unsubscribe = postsRef.onSnapshot(async (snapshot) => {
+      let changes = snapshot.docChanges();
+      let postsSnapshot = Posts;
+      changes.forEach((change) => {
+        console.log('Snapshot')
+        if (change.type == 'added') {
+          postsSnapshot.unshift({
+            pid: change.doc.id,
+            uid: change.doc.data().uid,
+            content: change.doc.data().content,
+            date: change.doc.data().date.toDate(),
+            image: change.doc.data().image,
+            likes: change.doc.data().like,
+            commentID: change.doc.data().commentID,
+          });
+
+          if (!usersOnPost.includes(change.doc.data().uid)) {
+            let a = usersOnPost;
+            a.push(change.doc.data().uid);
+            setUsersOnPost(a);
+          }
+        } else if (change.type == 'removed') {
+          console.log('Delete trigger');
+          setPosts(Posts.filter(el => el.pid === change.doc.id));
+        }
+      });
+
+      const usersRef = await FirebaseController.db
+        .collection("users")
+        .where("uid", "in", usersOnPost)
+        .get();
+
+      const usersSnapshot = usersRef.docs.map((doc) => ({
+        uid: doc.id,
+        displayName: doc.data().displayName,
+        avatarURL: doc.data().avatarURL,
+      }));
+
+      const data = [];
+      console.log('Update: ', postsSnapshot.length);
+      postsSnapshot.forEach((record) => {
+        // console.log(record);
+        record.displayName = usersSnapshot.find(
+          (e) => e.uid === record.uid
+        ).displayName;
+        record.avatarURL = usersSnapshot.find(
+          (e) => e.uid === record.uid
+        ).avatarURL;
+        data.push(record);
+      });
+      data.sort((a, b) => (a.date < b.date) ? 1 : -1);
+      setPosts(data);
+
+    });
+    //remember to unsubscribe from your realtime listener on umount or you will create a memory leak
+    return () => unsubscribe();
+  }, [type]);
+
+  // useEffect(() => {
+  //   getPosts();
+  // }, []);
 
   const getPosts = async () => {
     const postsRef =
       type == "profile"
         ? await FirebaseController.db
-            .collection("posts")
-            .where("uid", "==", urlUid)
-            .orderBy("date", "desc")
-            .get()
+          .collection("posts")
+          .where("uid", "==", urlUid)
+          .orderBy("date", "desc")
+          .get()
         : type == "search"
-        ? await FirebaseController.db
+          ? await FirebaseController.db
             .collection("posts")
             .orderBy("date", "desc")
             .get()
-        : await FirebaseController.db
+          : await FirebaseController.db
             .collection("posts")
             .orderBy("date", "desc")
             .get();
@@ -76,8 +152,8 @@ const Feed = (props) => {
         {type === "home" || currentUID === urlUid ? (
           <UploadPost />
         ) : (
-          <div></div>
-        )}
+            <div></div>
+          )}
       </Col>
       {Posts.map((post, idx) => {
         const permission = post.uid === currentUID || isAdmin ? true : false;
